a5c5d6d32856e1380b471adeb82267e8a5b3cf0a
OMI786I <sohrawardy1998@gmail.com>
2024-07-11 14:59:47 +0600

    getting data according to data id

--- index.js
+++ index.js
@@ -140,6 +140,15 @@ async function run() {
       res.send(result);
     });
 
+    //getting data according to _id
+
+    app.get("/submission/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await createdSubmissionCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
