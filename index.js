a96688b2adf44f854e3b6a8643f0886ab450db5f
OMI786I <sohrawardy1998@gmail.com>
2024-10-06 02:51:50 +0600

    Load data based on the page number

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://reset-assignment-11.web.app",
+      "https://reset-assignment-11.web.app",
+    ],
     credentials: true,
   })
 );
@@ -34,13 +38,13 @@ const client = new MongoClient(uri, {
 //middlewares
 
 const logger = (req, res, next) => {
-  console.log("log: info", req.method, req.url);
+  //console.log("log: info", req.method, req.url);
   next();
 };
 
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
-  console.log("token in the middle wear", token);
+  //console.log("token in the middle wear", token);
   if (!token) {
     return res.status(401).send({ message: "unauthorized access" });
   }
@@ -53,32 +57,34 @@ const verifyToken = (req, res, next) => {
   });
 };
 
+const cookieOption = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     //auth related api
 
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      console.log("user", user);
+      //console.log("user", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        })
-        .send({ success: true });
+      res.cookie("token", token, cookieOption).send({ success: true });
     });
 
     app.post("/logout", async (req, res) => {
       const user = req.body;
-      console.log("logging out", user);
-      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+      //console.log("logging out", user);
+      res
+        .clearCookie("token", { ...cookieOption, maxAge: 0 })
+        .send({ success: true });
     });
 
     //1. for creating assignment
@@ -90,7 +96,8 @@ async function run() {
     //sending on server ie. post
     app.post("/createdAssignment", async (req, res) => {
       const newCollection = req.body;
-      console.log(newCollection);
+      //console.log(newCollection);
+
       const result = await createdAssignmentCollection.insertOne(newCollection);
       res.send(result);
     });
@@ -99,10 +106,17 @@ async function run() {
 
     app.get("/createdAssignment", async (req, res) => {
       let query = {};
+      const page = parseInt(req.query.page) || 0;
+      const size = parseInt(req.query.size) || 10;
+
+      console.log(req);
       if (req.query?.difficulty) {
         query = { difficulty: req.query.difficulty };
       }
-      const cursor = createdAssignmentCollection.find(query);
+      const cursor = createdAssignmentCollection
+        .find(query)
+        .skip(page * size)
+        .limit(size);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -152,7 +166,7 @@ async function run() {
         options
       );
       res.send(result);
-      console.log(result);
+      //console.log(result);
     });
 
     //2. for submission assignment
@@ -164,7 +178,7 @@ async function run() {
     //sending on server ie. post
     app.post("/submission", async (req, res) => {
       const newCollection = req.body;
-      console.log(newCollection);
+      //console.log(newCollection);
       const result = await createdSubmissionCollection.insertOne(newCollection);
       res.send(result);
     });
@@ -217,14 +231,13 @@ async function run() {
         options
       );
       res.send(result);
-      console.log(result);
+      //console.log(result);
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    //await client.db("admin").command({ ping: 1 });
+    //console.log(
+    ("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -237,5 +250,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
+  //console.log(`server is running on port: ${port}`);
 });
